eb3c17bff82b0059bcfd39a4851c20dfb463f064
Alina Digtyar <alinadigtyar@gmail.com>
2016-12-15 20:18:37 +0200

    final commit

---
+++ src/scripts/main.js
@@ -0,0 +1,9 @@
+;(function(){
+	"use strict";
+
+	var firstNumber = prompt("Enter first number"),
+		secondNumber = prompt("Enter second number");
+
+	console.log( Number(firstNumber) + Number(secondNumber) );
+	
+})();
